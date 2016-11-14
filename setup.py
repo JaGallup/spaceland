08899a90ff7da5202074e575a603c45e85c1a782
Matt Riggott <matt@ja.is>
2016-11-14 22:30:40 +0000

    Create Setuptools-based package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(name="Spaceland",
+      version="0.0.1",
+      description="Python 3 library for fast, Pythonic access to shapefiles",
+      author="Matt Riggott",
+      author_email="matt@ja.is",
+      url="https://github.com/JaGallup/spaceland",
+      packages=["spaceland"],
+      license="MIT")
