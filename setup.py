8451d2cd015c904c3ddfadc473a447e45337e635
Matt Riggott <matt@ja.is>
2016-11-14 22:35:35 +0000

    Change version to 0.1.0-dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name="Spaceland",
-      version="0.0.1",
+      version="0.1.0-dev",
       description="Python 3 library for fast, Pythonic access to shapefiles",
       author="Matt Riggott",
       author_email="matt@ja.is",
